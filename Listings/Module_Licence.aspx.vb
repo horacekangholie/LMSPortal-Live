9c5be8332c2f1a1b512829d831282d78a9f72d27
dmcamsd1g1 <dmcamsd1g1@gmail.com>
2025-12-01 17:06:01 +0800

    Minor edit

--- Listings/Module_Licence.aspx.vb
+++ Listings/Module_Licence.aspx.vb
@@ -189,10 +189,10 @@ Partial Class Listings_Module_Licence
                                   " FROM I_LMS_Module_Licence " &
                                   " LEFT JOIN LMS_Module_Licence_Activated ON LMS_Module_Licence_Activated.[Licence_Code] = REPLACE(I_LMS_Module_Licence.[Licence Code], '-', '') " &
                                   " WHERE [Customer ID] = '" & Customer_ID & "'" &
-                                  "   AND [PO No] = N'" & PO_No & "'"
+                                  "   AND [PO No] = '" & PO_No & "'"
             Response.Write(query)
             If PO_No <> "NA" Then
-                query += " AND [PO Date] = '" & PO_Date & "' "
+                query += " AND [PO Date] = N'" & PO_Date & "' "
             Else
                 query += " AND [Created Date] = '" & Created_Date & "' "
             End If
