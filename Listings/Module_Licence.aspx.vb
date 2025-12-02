5a5e1ff0b0ee966a32416c0b8b4c57aeb5df7d78
dmcamsd1g1 <dmcamsd1g1@gmail.com>
2025-12-02 08:24:44 +0800

    Minor edit

--- Listings/Module_Licence.aspx.vb
+++ Listings/Module_Licence.aspx.vb
@@ -174,7 +174,7 @@ Partial Class Listings_Module_Licence
             Dim PO_Date As String = e.Row.Cells(GetColumnIndexByName(e.Row, "PO Date")).Text
             Dim Licence_Code As GridView = TryCast(e.Row.FindControl("gvLicenceList"), GridView)   '' gvLicenceList shared common both App / Product Licence and Module Licence
             Dim Created_Date As String = e.Row.Cells(GetColumnIndexByName(e.Row, "Created Date")).Text
-
+            Response.Write(PO_Date)
             If Replace(PO_Date, "&nbsp;", "") = "" Then
                 PO_Date = ""
             Else
